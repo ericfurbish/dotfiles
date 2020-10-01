f451b55a07df7af58f0892ea67660ad0d0c394fa
Eric Furbish <eric.furbish@tylertech.com>
2020-10-01 00:40:27 +0000

    Create installExtension function

--- install.sh
+++ install.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 env
 cat ~/.dotfiles/.bashrc >> ~/.bashrc
-SESSIONID=`ls /home/genero/.vscode-server/bin`
-/home/genero/.vscode-server/bin/$SESSIONID/server.sh --extensions-download-dir /home/genero/.vscode-server/extensionsCache --install-extension ms-kubernetes-tools.vscode-kubernetes-tools --force
+
+function installExtension()
+{
+  SESSIONID=`ls $HOME/.vscode-server/bin`
+  $HOME/.vscode-server/bin/$SESSIONID/server.sh --extensions-download-dir $HOME/.vscode-server/extensionsCache --install-extension $1 --force
+  EXTENSION=`ls $HOME/.vscode-server/extensionsCache/$1-*`
+  EXTENSION=`basename $EXTENSION`
+  mkdir /tmp/$EXTENSION
+  unzip -qq $HOME/.vscode-server/extensionsCache/$EXTENSION extension/* -d /tmp/$EXTENSION
+  mkdir -p $HOME/.vscode-server/extensions
+  mv /tmp/$EXTENSION/extension/ $HOME/.vscode-server/extensions/$EXTENSION
+}
+
+installExtension ms-kubernetes-tools.vscode-kubernetes-tools
